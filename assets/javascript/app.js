05131657b861ef26ecf735eb9a14268160ed6aa6
PocketThi3f <krslai@yahoo.com>
2016-10-25 02:48:08 -0700

    hairy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,51 @@
-var timer = {
-
-
-
-}
+var timeLeft = 5;
+var correctAnswers = 0;
+var wrongAnswers = 0;
 
 
 function countDownTimer() {
 
+	runLow = setInterval(goingDown, 1000);
+
+}
+
+
+function goingDown() {
+
+	timeLeft--;
+
+		$('#countDown').html('<h2>You have '+timeLeft+' seconds left!</h2>');
+
+	if (timeLeft < 1) {
+
+		staphIt();
+
+		$('#countDown').html('<h1>Oops! Time is up!</h1>');
+	}
+}
+
+countDownTimer();
+
+
+function staphIt() {
+
+	clearInterval(runLow);
+
+}
+
+
+function scoreCalc() {
+
+	if () {
+
+	} 
+
+	else () {
+
+
+	}
+
 
 
 }
 
-$("#countDown")
